dc8808defd93bce8fefba57f20319bfb4bc5c2b6
Meghan Mullally <ms.mullally27@gmail.com>
2019-04-18 19:15:02 -0700

    modications in class

--- lesson03/activity/index.js
+++ lesson03/activity/index.js
@@ -1,20 +1,39 @@
 class dog {
-  constructor(name, age, breed, color, barkLevel, energyLevel, pat) {
+  constructor(name, age, breed, color, barkLevel, energyLevel, patMethod) {
     this.name = name;
     this.age = age;
     this.breed = breed;
     this.color = color;
-    this.barkLevel = 10;
-    this.energyLevel = 0;
+    this.barkLevel = barkLevel;
+    this.energyLevel = energyLevel;
   }
 
-  // Pat method 
 
-  pat() {
-    this.energyLevel++
-    this.barkLevel--;
-
-    console.log(`This dog's name is ${this.name}, age is ${this.age}, current energy level is ${this.energyLevel} and the bark level is ${this.barkLevel}`);
+   patMethod() {
+     return this.energyLevel++
+    //  this.barkLevel--;
+   }
   }
-}
 
+  const dog1 = new dog('rosie', 13, 'baset hound/lab', 'brown white', 5, 8);
+  console.log(dog1);
+  dog1.patMethod();
+  
+
+
+
+    console.log(`This dog's name is ${this.name}, age is ${this.age}, current energy level is ${this.energyLevel} and the bark level is ${this.barkLevel}.`);
+  
+
+
+// // Create new dog 
+
+// const Penny = new dog('Penny', 13, 'Carin Terrior', 'brown');
+
+// // Pat method 
+
+// Penny.patMethod();
+// Penny.patMethod();
+// Penny.patMethod();
+// Penny.patMethod();
+// Penny.patMethod();
