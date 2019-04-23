43d9c16a852fadadb2ec63782578835a733dd37d
Meghan Mullally <ms.mullally27@gmail.com>
2019-04-22 22:15:39 -0700

    put the json code in the js file but it is still saying i have a jquery.deffered exceptioni typeerror and an uncaught error...

--- lesson03/assignment/data.js
+++ lesson03/assignment/data.js
@@ -1,23 +1,64 @@
 
-$(function () {
+$(document).ready(function() {
+  const data = 
+  [
+      {
+      "name": "Steve Smith",
+      "jobTitle": "Project Manage",
+      "Company": "Front End Dev Co",
+      "Experience": "3 years",
+      "School": "UW",
+      "Major": "Marketing",
+      "Email": "steve@fedc.com",
+      "LinkedInUrl": "steve.linkedinprofile.com"
+    }, {
+      "name": "Aaron Katz",
+      "jobTitle": "Full Stack Developer",
+      "Company": "Web Sites and More",
+      "Experience": "5 years",
+      "School": "SU",
+      "Major": "Computer Science",
+      "Email": "aaronNotMyEmail@uw.com",
+      "LinkedInUrl": "aaron.linkedinprofile.com"
+    }, {
+      "name": "Kyle Hendricks",
+      "jobTitle": "Starting Pitcher",
+      "Company": "Chicago Cubs",
+      "Experience": "12 years",
+      "School": "USC",
+      "Major": "Pitching",
+      "Email": "kyleH@cubs.com",
+      "LinkedInUrl": "kyle.linkedinprofile.com"
+    },
+    {
+      "name": "Michael Jordan",
+      "jobTitle": "Point Guard",
+      "Company": "Chicago Buls",
+      "Experience": "20 years",
+      "School": "UNC",
+      "Major": "Trash Talking",
+      "Email": "mJordan@bulls.com",
+      "LinkedInUrl": "mJordan.linkedinprofile.com"
+    }
+    ];
 
-  let dataHTML = data.map(function(n) {
-    let data = 
+  let dataHTML = data.map(function(i) {
+    return data = 
    `<div class="right">
     <img class="profile" src="img/unsplash-headshot.jpg" alt="Steve Smith's profile picture">
     <div class="name">
-      <h2 class="data_name">${n.name}</h2>
-      <h4 class="job_title">${n.job_title}</h4>
+      <h2 class="data_name">${i.name}</h2>
+      <h4 class="job_title">${i.job_title}</h4>
     </div>
   </div>
   <div class="info">
-    <p class="data_company"><span class="bold">Company:</span> ${n.company}</p>
-    <p class="data_experience"><span class="bold">Experience:</span> ${n.experience}</p>
-    <p> class="data_school"<span class="bold">${n.school}</span> UW</p>
-    <p class="data_major"><span class="bold">${n.major}</span> Marketing</p>
-    <p class="data_email"> <span class="bold">${n.email}</span> steve@fedc.com</p>
+    <p class="data_company"><span class="bold">Company:</span> ${i.company}</p>
+    <p class="data_experience"><span class="bold">Experience:</span> ${i.experience}</p>
+    <p> class="data_school"<span class="bold">${i.school}</span> UW</p>
+    <p class="data_major"><span class="bold">${i.major}</span> Marketing</p>
+    <p class="data_email"> <span class="bold">${i.email}</span> steve@fedc.com</p>
     <img class="linkedin" src="img/linkedin.svg" alt="linkedin-logo">
-    <p class="data_LinkedinURL">${n.linkedinUrl}</p>
+    <p class="data_LinkedinURL">${i.linkedinUrl}</p>
   </div>`;
 
     $(".data").append(dataHTML);
