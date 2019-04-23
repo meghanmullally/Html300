c790ce2996dc6e1b9501fd914b9da9b3b83e17ef
Meghan Mullally <ms.mullally27@gmail.com>
2019-04-22 21:42:05 -0700

    data into js, deleted js code and added script

---
+++ lesson03/assignment/data.js
@@ -0,0 +1,23 @@
+$(function(){
+
+  const dataHTML = data.map(function(i)) {
+    let data = `<div class="right">
+    <img class="profile" src="img/unsplash-headshot.jpg" alt="Steve Smith's profile picture">
+    <div class="name">
+      <h2>Steve Smith</h2>
+      <h4>Project Manager</h4>
+    </div>
+  </div>
+  <div class="info">
+    <p><span class="bold">Company:</span> Front End Dev Co</p>
+    <p><span class="bold">Experience:</span> 3 years</p>
+    <p><span class="bold">School:</span> UW</p>
+    <p><span class="bold">Major:</span> Marketing</p>
+    <p> <span class="bold">Email:</span> steve@fedc.com</p>
+    <img class="linkedin" src="img/linkedin.svg" alt="linkedin-logo">
+    <p>steve.linkedinprofile.com</p>
+  </div>`;
+  }
+
+
+})
